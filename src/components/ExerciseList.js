015e0a0a7e668fb3c46e0d5e1cdb27c471fa8ae4
eddygld <61808648+eddygld@users.noreply.github.com>
2021-03-10 18:40:58 -0500

    exercise list component almost done

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const ExerciseList = () => {
-    return (
-        <div>
-            You are on the exercise list component
-        </div>
-    )
+class ExerciseList extends Component {
+
+    state = {
+        username: '',
+        description: '',
+        duration: 0,
+        date: new Date(),
+        users: []
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        });
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    handleDescriptionChange = (e) => {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    handleDurationChange = (e) => {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    handleDateChange = (date) => {
+        this.setState({
+            date: date
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+
+        window.location = '/';
+
+
+    }
+    
+    render() {
+        return (
+            <div>
+                You are on the exercise list component
+            </div>
+        )    
+    }
+    
 }
 
 export default ExerciseList
