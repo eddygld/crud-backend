ff8b62f1d37e0413c10ebdefe40926efc4807f86
eddygld <61808648+eddygld@users.noreply.github.com>
2021-03-12 16:37:03 -0500

    form-control

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,62 +1,6 @@
 import React, { Component } from 'react';
 
 class ExerciseList extends Component {
-
-    state = {
-        username: '',
-        description: '',
-        duration: 0,
-        date: new Date(),
-        users: []
-    }
-
-    componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        });
-    }
-
-    handleUsernameChange = (e) => {
-        this.setState({
-            username: e.target.value
-        });
-    }
-
-    handleDescriptionChange = (e) => {
-        this.setState({
-            description: e.target.value
-        });
-    }
-
-    handleDurationChange = (e) => {
-        this.setState({
-            duration: e.target.value
-        });
-    }
-
-    handleDateChange = (date) => {
-        this.setState({
-            date: date
-        });
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-
-        const exercise = {
-            username: this.state.username,
-            description: this.state.description,
-            duration: this.state.duration,
-            date: this.state.date
-        }
-
-        console.log(exercise);
-
-        window.location = '/';
-
-
-    }
     
     render() {
         return (
--- src/components/LogExercise.js
+++ src/components/LogExercise.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const CreateExercise = () => {
-    return (
-        <div>
-            You are on the log exercise component
-        </div>
-    )
+class LogExercise extends Component {
+
+    state = {
+        username: '',
+        description: '',
+        duration: 0,
+        date: new Date(),
+        users: []
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['test user'],
+            username: 'test user'
+        });
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    handleDescriptionChange = (e) => {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    handleDurationChange = (e) => {
+        this.setState({
+            duration: e.target.value
+        });
+    }
+
+    handleDateChange = (date) => {
+        this.setState({
+            date: date
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+
+        console.log(exercise);
+
+        window.location = '/';
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h3>Create New Exercise Log</h3>
+                <form onSubmit={this.handleSubmit} >
+                    <div>
+                        <label>Username: </label>
+                        <select 
+                            ref="userInput"
+                            required
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange}>
+                            { this.state.users.map( user => 
+                            <option 
+                                key={user} 
+                                value={user}>
+                                    {user}
+
+                            </option> ) }
+                        </select>
+                    </div>
+                    <div>
+                        <label>Description: </label>
+                        <input
+                            type="text"
+                            value={this.state.description}
+                            onChange={this.handleDescriptionChange} 
+                        />
+                    </div>
+                    <div>
+                        <label>Duration (in minutes): </label>
+                        <input
+                            type="text"
+                            value={this.state.duration}
+                            onChange={this.handleDurationChange} 
+                        />
+                    </div>
+                    <div>
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.handleDateChange} 
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <input type="submit" value="Create Exercise Log" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+
 }
 
-export default CreateExercise
+export default LogExercise
